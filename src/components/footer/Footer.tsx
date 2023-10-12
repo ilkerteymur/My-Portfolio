a63679b3eb57301cbab8dc2ba7f88ea3e8cdd872
ilkerteymur <ilkerteymuroglu@gmail.com>
2023-10-12 13:22:24 +0300

    bug fixed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <h1 className="footer__title">Ilker TEYMUROGLUUU</h1>
+        <h1 className="footer__title">Ilker TEYMUROGLU</h1>
 
         <ul className="footer__list">
           <li>
